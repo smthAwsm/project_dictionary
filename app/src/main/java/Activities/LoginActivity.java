f580946b992e2f47b67536460f4a4e01231abf34
smth_awsm <petrenkobest@gmail.com>
2016-07-10 21:56:21 +0300

    added Google Drive API

--- app/src/main/java/Activities/LoginActivity.java
+++ app/src/main/java/Activities/LoginActivity.java
@@ -1,17 +1,35 @@
 package activities;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.study.xps.projectdictionary.R;
+
+import java.util.List;
+
+import Helpers.GoogleDriveHelper;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by XPS on 4/9/2016.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "JUST_LEARN_IT_DICTIONARY";
+    private final LoginActivity mContext = this;
+    private GoogleDriveHelper mGoogleDriveHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +40,66 @@ public class LoginActivity extends Activity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),DictionariesActivity.class));
+                Toast.makeText(getApplicationContext(),"TOUCHED",Toast.LENGTH_SHORT).show();
+
+                if(mGoogleDriveHelper == null)
+               mGoogleDriveHelper  = new GoogleDriveHelper(mContext);
+                else mGoogleDriveHelper.callApi();
+
+               // startActivity(new Intent(getApplicationContext(),DictionariesActivity.class));
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    switch (requestCode){
+        case REQUEST_GOOGLE_PLAY_SERVICES:
+            if(requestCode == RESULT_OK ){
+                Toast.makeText(
+                        this,
+                        getString(R.string.no_play_services),
+                        Toast.LENGTH_SHORT
+                ).show();
+            } else mGoogleDriveHelper.callApi();
+            break;
+        case REQUEST_ACCOUNT_PICKER:
+            if(resultCode == RESULT_OK && data!=null && data.getExtras() != null){
+                String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if(accountName != null){
+                        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString(PREF_ACCOUNT_NAME,accountName);
+                        editor.apply();
+                        mGoogleDriveHelper.setAccountName(accountName);
+                        mGoogleDriveHelper.callApi();
+                    }
+            }
+            break;
+        case REQUEST_AUTHORIZATION:
+            if(resultCode == RESULT_OK){
+                mGoogleDriveHelper.callApi();
+            }
+            break;
+    }
+}
+
+    //region PermissionCallbacks
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+//endregion
 }
---
+++ app/src/main/java/Helpers/GoogleDriveHelper.java
@@ -0,0 +1,121 @@
+package Helpers;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.study.xps.projectdictionary.R;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.jar.Manifest;
+
+import activities.LoginActivity;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+/**
+ * Created by XPS on 07/10/2016.
+ */
+public class GoogleDriveHelper {
+
+
+    private GoogleAccountCredential mAccountCredential;
+    private Activity mContext;
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+    private static final String PREF_ACCOUNT_NAME = "JUST_LEARN_IT_DICTIONARY";
+    private static final String[] SCOPES = {DriveScopes.DRIVE_METADATA_READONLY};
+
+    public GoogleDriveHelper(Activity context){
+                this.mContext = context;
+        mAccountCredential = GoogleAccountCredential.usingOAuth2(
+                mContext,
+                Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+        callApi();
+    }
+
+    public void callApi(){
+        if(!isGooglePlayAvailable()){
+            acquireGooglePlayServices();
+        } else if(mAccountCredential.getSelectedAccountName() == null){
+            chooseAccout();
+        } else if (!isDeviceOnline()) {
+            Toast.makeText(mContext, mContext.getString(R.string.no_inet), Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(mContext,"TASK START",Toast.LENGTH_LONG).show();
+        //else new MakeRequestTask
+    }
+
+    private boolean isGooglePlayAvailable(){
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        final int connectionStatusCode = googleApiAvailability.isGooglePlayServicesAvailable(mContext);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+    private void acquireGooglePlayServices(){
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        final int connectionStatusCode = googleApiAvailability.isGooglePlayServicesAvailable(mContext);
+        if(googleApiAvailability.isUserResolvableError(connectionStatusCode))
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+    }
+    private void showGooglePlayServicesAvailabilityErrorDialog(final int connectionStatusCode){
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = googleApiAvailability.getErrorDialog(
+                mContext,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
+    }
+
+    private boolean isDeviceOnline(){
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager .getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccout(){
+        if(EasyPermissions.hasPermissions(mContext, android.Manifest.permission.GET_ACCOUNTS)){
+            String accountName = mContext.getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME,null);
+            if(accountName != null){
+                mAccountCredential.setSelectedAccountName(accountName);
+                callApi();
+            } else {
+                mContext.startActivityForResult(
+                        mAccountCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+                EasyPermissions.requestPermissions(
+                        mContext,
+                        mContext.getString(R.string.account_permission),
+                        REQUEST_PERMISSION_GET_ACCOUNTS,
+                        android.Manifest.permission.GET_ACCOUNTS);
+            }
+        }
+
+    public void setAccountName(String accountName) {
+        mAccountCredential.setSelectedAccountName(accountName);
+    }
+
+}
+
+
